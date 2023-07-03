07e6aa288c93412f1afece22636c688f6ee1b5d9
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-07-04 00:03:12 +0700

    update categories

--- src/components/home/Categories.vue
+++ src/components/home/Categories.vue
@@ -1,5 +1,12 @@
 <script setup>
 import CategoriesCardVue from "./../CategoriesCard.vue";
+
+const categories = ref([
+  { id: 1, title: "Mobile UI Kit", count: 731, image: "categories-1.jpg" },
+  { id: 2, title: "font", count: 657, image: "categories-2.jpg" },
+  { id: 3, title: "icon Set", count: 83559, Image: "categories-3.jpg" },
+  { id: 4, title: "Website Ui Kit", count: 4500, Image: "categories-4.jpg" },
+]);
 </script>
 <template>
   <div class="container px-4 mx-auto my-16 md:px-12">
