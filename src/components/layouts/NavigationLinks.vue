d30a92150fd59e5449c7c60698976fd95bf03b59
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-07-04 12:37:19 +0700

    update auth login and register

--- src/components/layouts/NavigationLinks.vue
+++ src/components/layouts/NavigationLinks.vue
@@ -1,34 +1,42 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
-    <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-regular">
+  <div
+    class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+    id="mobile-menu-2">
+    <ul
+      class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-regular">
       <li>
-        <RouterLink to="/"
+        <RouterLink
+          to="/"
           class="block py-2 pl-3 pr-4 text-white bg-indigo-600 rounded md:bg-transparent md:text-indigo-600 md:p-0 dark:text-white"
-          aria-current="page">Home
+          aria-current="page"
+          >Home
         </RouterLink>
       </li>
       <li>
-        <RouterLink to="/categories"
+        <RouterLink
+          to="/categories"
           class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-          Categories</RouterLink>
+          Categories</RouterLink
+        >
       </li>
       <li>
-        <RouterLink to="/pricing"
+        <RouterLink
+          to="/pricing"
           class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-          Pricing</RouterLink>
+          Pricing</RouterLink
+        >
       </li>
       <li>
-        <RouterLink to="/case"
+        <RouterLink
+          to="/case"
           class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-          Study
-          Case</RouterLink>
+          About</RouterLink
+        >
       </li>
     </ul>
   </div>
 </template>
-
-
