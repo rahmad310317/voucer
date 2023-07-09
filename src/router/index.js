5884a8266dd814a087037b81bd5084adfb5bfe21
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-07-09 11:12:48 +0700

    update formlogin and formRegister

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from "../views/HomeView.vue";
-import LoginView from "../views/LoginView.vue"
-import RegisterView from "../views/RegisterView.vue"
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,12 +14,12 @@ const router = createRouter({
     {
       path: "/login",
       name: "login",
-      component: LoginView,
+      component: () => import("../views/LoginView.vue")
     },
     {
       path: "/register",
       name: "register",
-      component: RegisterView,
+      component: () => import("../views/RegisterView.vue")
     },
   ]
 })
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,7 @@
 <script setup>
-import FormLogin from "@/components/authentication/LoginForm.vue";
+import FormLoginVue from "../components/authentication/FormLogin.vue";
 </script>
 
 <template>
-  <div class="relative overflow-hidden bg-white">
-    <div class="mx-auto max-w-7xl">
-      <div
-        class="flex flex-col items-center w-full min-h-screen pt-6 sm:justify-center sm:pt-0">
-        <!-- login Form -->
-        <FormLogin />
-        <div class="w-full mx-auto mt-20 sm:max-w-2xl">
-          <img src="img/brand-logo.svg" alt="" class="w-full px-10 md:px-0" />
-        </div>
-      </div>
-    </div>
-  </div>
+  <FormLoginVue />
 </template>
